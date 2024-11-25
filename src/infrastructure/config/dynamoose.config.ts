59cba7375fd0107fb76153e87403e3555eef5e5f
spiritbear <cardenascode7@gmail.com>
2024-11-24 23:06:47 -0500

    se actualizo las configuraciones para deployar en aws

--- src/infrastructure/config/dynamoose.config.ts
+++ src/infrastructure/config/dynamoose.config.ts
@@ -2,16 +2,16 @@ import * as dynamoose from 'dynamoose';
 import { ConfigService } from '@nestjs/config';
 
 export const DynamooseConfig = (configService: ConfigService) => {
-  const region = configService.get<string>('AWS_REGION') || 'us-east-2';
+  const region = configService.get<string>('AWS_RGN') || 'us-east-2';
 
   const ddb = new dynamoose.aws.ddb.DynamoDB({
     region,
     //endpoint,
     credentials: {
       accessKeyId:
-        configService.get<string>('AWS_ACCESS_KEY_ID') || 'fakeAccessKeyId',
+        configService.get<string>('AWS_ACS_KEY_ID') || 'fakeAccessKeyId',
       secretAccessKey:
-        configService.get<string>('AWS_SECRET_ACCESS_KEY') ||
+        configService.get<string>('AWS_SECRET_ACS_KEY') ||
         'fakeSecretAccessKey',
     },
   });
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,32 @@
+import { configure as serverlessExpress } from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedServer;
 
-  app.enableCors();
+export const handler = async (event, context) => {
+  if (!cachedServer) {
+    const nestApp = await NestFactory.create(AppModule);
 
-  // Configuración de Swagger
-  const config = new DocumentBuilder()
-    .setTitle('StarWars API Hexagonal')
-    .setDescription('API para gestionar personajes favoritos e integrar SWAPI')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('StarWars API Hexagonal')
+      .setDescription(
+        'API para gestionar personajes favoritos e integrar SWAPI',
+      )
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(nestApp, config);
+    SwaggerModule.setup('api-docs', nestApp, document);
 
-  // Aplicar el filtro de excepciones global
-  app.useGlobalFilters(new AllExceptionsFilter());
+    nestApp.useGlobalFilters(new AllExceptionsFilter());
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Aplicación corriendo en http://localhost:${port}`);
-  console.log(
-    `Documentación de Swagger disponible en http://localhost:${port}/api-docs`,
-  );
-}
+    await nestApp.init();
+    cachedServer = serverlessExpress({
+      app: nestApp.getHttpAdapter().getInstance(),
+    });
+  }
 
-bootstrap();
+  return cachedServer(event, context);
+};
